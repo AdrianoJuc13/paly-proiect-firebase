cb2a1dd6b51f9ad10c44fdd4a6092034c932a736
AndreiMorosan <morosan_andrei20@yahoo.com>
2022-10-25 20:40:18 +0300

    Andrei fix

--- src/views/AdaugaComanda/AdaugaComanda.js
+++ src/views/AdaugaComanda/AdaugaComanda.js
@@ -1,11 +1,11 @@
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import { database } from "../../firebaseConfig";
 import { collection, addDoc } from "firebase/firestore";
-import { useState } from "react";
 import styles from "./styles.module.scss";
 import Layout from "../../components/Layout/Layout";
 import { v4 as uuidv4 } from "uuid";
 import { MinusOutlined } from "@ant-design/icons";
-import { useNavigate } from "react-router-dom";
 
 export default function AdaugaComanda() {
   const navigate = useNavigate();
@@ -25,6 +25,8 @@ export default function AdaugaComanda() {
       pret: "",
     },
   ]);
+
+  const pdfRef = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -90,9 +92,23 @@ export default function AdaugaComanda() {
 
   // const saveComanda = () => {};
 
+  const submitDownload = async () => {
+    const content = pdfRef.current;
+    const opt = {
+      margin: 0,
+      filename: `Comanda client ${client}.pdf`,
+      image: { type: "jpeg", quality: 0.95 },
+      pagebreak: { mode: "avoid-all" },
+      html2canvas: { useCORS: true, allowTaint: true },
+      // jsPDF: { compress: true },
+    };
+    const html2pdf = (await import("html2pdf.js")).default;
+    html2pdf(content, opt);
+  };
+
   return (
     <Layout>
-      <div className={styles.screen}>
+      <div ref={pdfRef} className={styles.screen}>
         <div className={styles.titlu}>Creaza comanda</div>
         <div className={styles.container}>
           <div className={styles.field}>
@@ -127,90 +143,95 @@ export default function AdaugaComanda() {
           Adauga Produs
         </button>
         <div className={styles.ListaProduse}>
-          <form className={styles.form} onSubmit={handleSubmit}>
+          <form className={styles.form}>
             {inputFields.map((inputField, index) => (
               <div>
-                <h3 className={styles.textHeaderInput}>Produs numar {index+1}</h3>
+                <h3 className={styles.textHeaderInput}>
+                  Produs numar {index + 1}
+                </h3>
                 <div className={styles.row} key={inputField.id}>
-                <div className={styles.rowData}>
-                  <input
-                    className={styles.input}
-                    name="nume"
-                    placeholder="nume"
-                    value={inputField.nume}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                  <input
-                    className={styles.input}
-                    name="lungime"
-                    placeholder="lungime"
-                    value={inputField.lungime}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                  <input
-                    className={styles.input}
-                    name="latime"
-                    placeholder="latime"
-                    value={inputField.latime}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                  <input
-                    className={styles.input}
-                    name="grosime"
-                    placeholder="grosime"
-                    value={inputField.grosime}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                  <input
-                    className={styles.input}
-                    name="cantitate"
-                    placeholder="cantitate"
-                    value={inputField.cantitate}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                  <input
-                    className={styles.input}
-                    name="pret"
-                    placeholder="pret"
-                    value={inputField.pret}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                </div>
-                <div
-                  className={styles.removeBtn}
-                  disabled={inputFields.length === 1}
-                  onClick={() => handleRemoveFields(inputField.id)}
-                >
-                  <MinusOutlined />
+                  <div className={styles.rowData}>
+                    <input
+                      className={styles.input}
+                      name="nume"
+                      placeholder="nume"
+                      value={inputField.nume}
+                      onChange={(event) =>
+                        handleChangeInput(inputField.id, event)
+                      }
+                    />
+                    <input
+                      className={styles.input}
+                      name="lungime"
+                      placeholder="lungime"
+                      value={inputField.lungime}
+                      onChange={(event) =>
+                        handleChangeInput(inputField.id, event)
+                      }
+                    />
+                    <input
+                      className={styles.input}
+                      name="latime"
+                      placeholder="latime"
+                      value={inputField.latime}
+                      onChange={(event) =>
+                        handleChangeInput(inputField.id, event)
+                      }
+                    />
+                    <input
+                      className={styles.input}
+                      name="grosime"
+                      placeholder="grosime"
+                      value={inputField.grosime}
+                      onChange={(event) =>
+                        handleChangeInput(inputField.id, event)
+                      }
+                    />
+                    <input
+                      className={styles.input}
+                      name="cantitate"
+                      placeholder="cantitate"
+                      value={inputField.cantitate}
+                      onChange={(event) =>
+                        handleChangeInput(inputField.id, event)
+                      }
+                    />
+                    <input
+                      className={styles.input}
+                      name="pret"
+                      placeholder="pret"
+                      value={inputField.pret}
+                      onChange={(event) =>
+                        handleChangeInput(inputField.id, event)
+                      }
+                    />
+                  </div>
+                  <div
+                    className={styles.removeBtn}
+                    disabled={inputFields.length === 1}
+                    onClick={() => handleRemoveFields(inputField.id)}
+                  >
+                    <MinusOutlined />
+                  </div>
                 </div>
               </div>
-              </div>
-
             ))}
-
-            <div className={styles.endArea}>
-              <button
-                className={styles.saveBtn}
-                type="submit"
-                onClick={handleSubmit}
-              >
-                Salveaza
-              </button>
-              <button className={styles.printBtn}>Printeaza</button>
-            </div>
           </form>
+          <div className={styles.endArea}>
+            <button
+              className={styles.saveBtn}
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Salveaza
+            </button>
+            <button
+              onClick={() => submitDownload()}
+              className={styles.printBtn}
+            >
+              Printeaza
+            </button>
+          </div>
         </div>
       </div>
     </Layout>
